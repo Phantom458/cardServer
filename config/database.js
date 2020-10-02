3290c88cd47f7e751fa37e97fa69cf988e917f32
Phantom458 <68326607+Phantom458@users.noreply.github.com>
2020-10-03 00:38:01 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 //module.exports makes code ready to be called elsewhere
-module.exports = new Sequelize('cardapp', 'root', 'hellUvashit421//', {
-    host: 'localhost',
+module.exports = new Sequelize('heroku_91fbe24ccc62c72', 'b198b974120c73', '6c98148b', {
+    host: 'us-cdbr-iron-east-01.cleardb.net',
     dialect: 'mysql',
     define: {
       timestamps: false
